e8a0f3e47d1f7fd96345f740b53ec1ae83cdab4b
zqy <VDerGoW@hotmail.com>
2023-08-03 21:21:21 +0800

    feat: drag and drop

---
+++ Editor/Scripts/AssetItemView.cs
@@ -0,0 +1,209 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UDebug = UnityEngine.Debug;
+using UObject = UnityEngine.Object;
+
+namespace GBG.AssetQuickAccess.Editor
+{
+    internal class AssetItemView : IMGUIContainer
+    {
+        public static double DoubleClickInterval = 0.3f;
+
+        public Image Icon { get; }
+
+        public Label Title { get; }
+
+        public AssetHandle AssetHandle { get; set; }
+
+        public event System.Action<AssetHandle> OnWantsToRemoveAssetItem;
+
+        private MouseAction _mouseAction = MouseAction.None;
+
+        private double _lastClickTime;
+
+
+        public AssetItemView()
+        {
+            onGUIHandler = OnGUI;
+
+            // content
+            style.height = new Length(100, LengthUnit.Percent);
+            style.flexDirection = FlexDirection.Row;
+            style.backgroundColor = EditorGUIUtility.isProSkin
+                ? new Color(0.35f, 0.35f, 0.35f, 1.0f)
+                : new Color(0.9f, 0.9f, 0.9f, 1.0f);
+            // margin
+            style.marginLeft = 0;
+            style.marginRight = 0;
+            style.marginTop = 0;
+            style.marginBottom = 0;
+            // padding
+            style.paddingLeft = 32; // to avoid overlap with icon
+            style.paddingRight = 0;
+            style.paddingTop = 0;
+            style.paddingBottom = 0;
+            // border width
+            style.borderLeftWidth = 0;
+            style.borderRightWidth = 0;
+            style.borderTopWidth = 0;
+            style.borderBottomWidth = 0;
+            // border radius
+            style.borderTopLeftRadius = 0;
+            style.borderTopRightRadius = 0;
+            style.borderBottomLeftRadius = 0;
+            style.borderBottomRightRadius = 0;
+
+            Icon = new Image
+            {
+                pickingMode = PickingMode.Ignore,
+                style =
+                {
+                    width = new StyleLength(24),
+                    height = new Length(100, LengthUnit.Percent),
+                    marginLeft = -28 // to avoid overlap with text
+                }
+            };
+            Add(Icon);
+
+            Title = new Label
+            {
+                pickingMode = PickingMode.Ignore,
+                style =
+                {
+                    unityTextAlign = new StyleEnum<TextAnchor>(TextAnchor.MiddleLeft),
+                    textOverflow = new StyleEnum<TextOverflow>(TextOverflow.Ellipsis),
+                }
+            };
+            Add(Title);
+        }
+
+
+        // TODO FIXME: Will conflict with ListView itme drag action
+        private void OnGUI()
+        {
+
+            var e = Event.current;
+            switch (e.type)
+            {
+                case EventType.MouseDown:
+                    if (e.button == 0)
+                    {
+                        var currentTime = EditorApplication.timeSinceStartup;
+                        if (currentTime - _lastClickTime < DoubleClickInterval)
+                        {
+                            _mouseAction = MouseAction.DoubleClick;
+                            _lastClickTime = 0;
+                        }
+                        else
+                        {
+                            _mouseAction = MouseAction.Click;
+                            _lastClickTime = currentTime;
+                        }
+                        e.Use();
+                    }
+                    else if (e.button == 1)
+                    {
+                        _mouseAction = MouseAction.ContextClick;
+                        e.Use();
+                    }
+                    break;
+
+                case EventType.MouseUp:
+                    switch (_mouseAction)
+                    {
+                        case MouseAction.Click:
+                            if (e.button == 0)
+                            {
+                                _mouseAction = MouseAction.None;
+                                OnClick();
+                                e.Use();
+                            }
+                            break;
+
+                        case MouseAction.DoubleClick:
+                            if (e.button == 0)
+                            {
+                                _mouseAction = MouseAction.None;
+                                OnDoubleClick();
+                                e.Use();
+                            }
+                            break;
+
+                        case MouseAction.ContextClick:
+                            if (e.button == 1)
+                            {
+                                _mouseAction = MouseAction.None;
+                                OnContextClick(e.mousePosition);
+                                e.Use();
+                            }
+                            break;
+
+                        case MouseAction.None:
+                        case MouseAction.Drag: // If drag happens, the code can't reach here.
+                            break;
+
+                        default:
+                            throw new System.ArgumentOutOfRangeException(nameof(_mouseAction));
+                    }
+
+                    break;
+
+                case EventType.MouseDrag:
+                    if (_mouseAction == MouseAction.Click)
+                    {
+                        _mouseAction = MouseAction.Drag;
+                        DragAndDrop.PrepareStartDrag();
+                        DragAndDrop.objectReferences = new UObject[] { AssetHandle.Asset };
+                        DragAndDrop.paths = new string[] { AssetDatabase.GetAssetPath(AssetHandle.Asset) };
+                        DragAndDrop.StartDrag(null);
+                    }
+                    break;
+
+                case EventType.DragUpdated:
+                    if (DragAndDrop.objectReferences.Length > 0)
+                    {
+                        DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void OnClick()
+        {
+            EditorGUIUtility.PingObject(AssetHandle.Asset);
+        }
+
+        private void OnDoubleClick()
+        {
+            AssetDatabase.OpenAsset(AssetHandle.Asset);
+        }
+
+        private void OnContextClick(Vector2 mousePosition)
+        {
+            var menu = new GenericDropdownMenu();
+            menu.AddItem("Open", false, () => AssetDatabase.OpenAsset(AssetHandle.Asset));
+            menu.AddItem("Ping", false, () => EditorGUIUtility.PingObject(AssetHandle.Asset));
+            menu.AddItem("Open", false, () => AssetDatabase.OpenAsset(AssetHandle.Asset));
+            menu.AddItem("Ping", false, () => EditorGUIUtility.PingObject(AssetHandle.Asset));
+            menu.AddItem("Print Guid", false, () => UDebug.Log(AssetHandle.Guid, AssetHandle.Asset));
+            menu.AddItem("Print Path", false, () => UDebug.Log(AssetDatabase.GUIDToAssetPath(AssetHandle.Guid), AssetHandle.Asset));
+            menu.AddItem("Show in Folder", false, () => EditorUtility.RevealInFinder(AssetDatabase.GUIDToAssetPath(AssetHandle.Guid)));
+            menu.AddItem("Remove", false, () => OnWantsToRemoveAssetItem?.Invoke(AssetHandle));
+            menu.DropDown(new Rect(this.LocalToWorld(mousePosition), Vector2.zero), this);
+        }
+
+
+        enum MouseAction : byte
+        {
+            None,
+            Click,
+            DoubleClick,
+            ContextClick,
+            Drag,
+        }
+    }
+}
--- Editor/Scripts/AssetQuickAccessWindow.cs
+++ Editor/Scripts/AssetQuickAccessWindow.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
-using UDebug = UnityEngine.Debug;
 using UObject = UnityEngine.Object;
 
 namespace GBG.AssetQuickAccess.Editor
@@ -189,68 +188,24 @@ namespace GBG.AssetQuickAccess.Editor
 
         private ListView _assetListView;
 
-        private static readonly string _assetIconElementName = "asset-quick-access__asset-icon-image";
-
         private static Texture _warningTexture;
 
 
         private VisualElement CreateNewAssetListItem()
         {
-            var button = new Button
-            {
-                style =
-                {
-                    // content
-                    unityTextAlign = new StyleEnum<TextAnchor>(TextAnchor.MiddleLeft),
-                    textOverflow = new StyleEnum<TextOverflow>(TextOverflow.Ellipsis),
-                    height = new Length(100, LengthUnit.Percent),
-                    // margin
-                    marginLeft = 0,
-                    marginRight = 0,
-                    marginTop = 0,
-                    marginBottom = 0,
-                    // padding
-                    paddingLeft = 32, // to avoid overlap with icon
-                    paddingRight = 0,
-                    paddingTop = 0,
-                    paddingBottom = 0,
-                    // border width
-                    borderLeftWidth = 0,
-                    borderRightWidth = 0,
-                    borderTopWidth = 0,
-                    borderBottomWidth = 0,
-                    // border radius
-                    borderTopLeftRadius = 0,
-                    borderTopRightRadius = 0,
-                    borderBottomLeftRadius = 0,
-                    borderBottomRightRadius = 0,
-                }
-            };
+            var view = new AssetItemView();
+            view.OnWantsToRemoveAssetItem += RemoveAsset;
 
-            var iconImg = new Image
-            {
-                name = _assetIconElementName,
-                style =
-                {
-                    width = new StyleLength(24),
-                    height = new Length(100, LengthUnit.Percent),
-                    marginLeft = -28 // to avoid overlap with text
-                }
-            };
-            button.Add(iconImg);
-
-            return button;
+            return view;
         }
 
         private void BindAssetListItem(VisualElement element, int index)
         {
-            var button = (Button)element;
+            var view = (AssetItemView)element;
             var assetHandle = AssetQuickAccessSettings.GetAssetHandle(index);
-            button.text = assetHandle.GetDisplayName();
-            button.RegisterCallback<ClickEvent, AssetHandle>(OnClickAssetListItem, assetHandle);
-            button.RegisterCallback<ContextClickEvent, AssetHandle>(OnContextClickAssetListItem, assetHandle);
-
-            var iconImg = button.Q<Image>(_assetIconElementName);
+            view.AssetHandle = assetHandle;
+            view.Title.text = assetHandle.GetDisplayName();
+            view.tooltip = AssetDatabase.GetAssetPath(assetHandle.Asset);
             Texture iconTex = AssetPreview.GetMiniThumbnail(assetHandle.Asset);
             if (!iconTex)
             {
@@ -261,44 +216,23 @@ namespace GBG.AssetQuickAccess.Editor
 
                 iconTex = _warningTexture;
             }
-
-            iconImg.image = iconTex;
+            view.Icon.image = iconTex;
         }
 
         private void UnbindAssetListItem(VisualElement element, int index)
         {
-            element.UnregisterCallback<ClickEvent, AssetHandle>(OnClickAssetListItem);
-            element.UnregisterCallback<ContextClickEvent, AssetHandle>(OnContextClickAssetListItem);
-        }
-
-        private void OnClickAssetListItem(ClickEvent e, AssetHandle handle)
-        {
-            e.StopPropagation();
-
-            EditorGUIUtility.PingObject(handle.Asset);
-            if (e.clickCount > 1)
-            {
-                AssetDatabase.OpenAsset(handle.Asset);
-            }
-        }
-
-        private void OnContextClickAssetListItem(ContextClickEvent e, AssetHandle handle)
-        {
-            e.StopPropagation();
-
-            var menu = new GenericDropdownMenu();
-            menu.AddItem("Open", false, () => AssetDatabase.OpenAsset(handle.Asset));
-            menu.AddItem("Ping", false, () => EditorGUIUtility.PingObject(handle.Asset));
-            menu.AddItem("Print Guid", false, () => UDebug.Log(handle.Guid, handle.Asset));
-            menu.AddItem("Print Path", false, () => UDebug.Log(AssetDatabase.GUIDToAssetPath(handle.Guid), handle.Asset));
-            menu.AddItem("Show in Folder", false, () => EditorUtility.RevealInFinder(AssetDatabase.GUIDToAssetPath(handle.Guid)));
-            menu.AddItem("Remove", false, () => _isViewDirty |= AssetQuickAccessSettings.RemoveAsset(handle));
-            menu.DropDown(new Rect(e.mousePosition, Vector2.zero), e.currentTarget as VisualElement);
+            var view = (AssetItemView)element;
+            view.AssetHandle = null;
         }
 
         private void OnReorderAsset(int fromIndex, int toIndex)
         {
             AssetQuickAccessSettings.ForceSave();
+        }
+
+        private void RemoveAsset(AssetHandle assetHandle)
+        {
+            _isViewDirty |= AssetQuickAccessSettings.RemoveAsset(assetHandle);
         }
 
         private void OnDragAndDropAssets(IList<string> assetPaths)
